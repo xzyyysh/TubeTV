d6cd56c4b7cdf1673841bd2bf178055c3abe62fd
xzyyysh <pockioow0@gmail.com>
2026-06-10 19:04:32 -0400

    few changes

--- tv/build.gradle.kts
+++ tv/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "io.tubetvlol.tubetv"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "io.tubetvlol.tubetv"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0.0"
 
